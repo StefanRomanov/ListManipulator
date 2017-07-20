e4d2f0ff0c756caf2b748dc42d68b779ff0b1fd3
Vafla <romanovsc2@gmail.com>
2017-07-21 01:55:19 +0300

    Added project files

---
+++ append.js
@@ -0,0 +1,12 @@
+define([],function (){
+    return function append(element,list,textField){
+        if(element.length > 1){
+            throw new Error('“Error: invalid command parameters”')
+        }
+        let string = element.toString();
+        list.push(string);
+        textField.value += list.join(' ') + "\n";
+    }
+});
+
+
---
+++ terminal.js
@@ -0,0 +1,37 @@
+define(['./append'],function(appendModule){
+
+    let functions = {
+        append: appendModule
+    };
+    let textField = document.getElementById('textarea');
+    let terminal = document.getElementById('terminal');
+
+    let list = [];
+    let initialized = false;
+
+    document.getElementById('submit').addEventListener('click',submit);
+    terminal.addEventListener('keypress', e => e.code === 'Enter' ? submit() : "");
+
+
+    function submit(){
+        let tokens = terminal.value.split(" ").filter(e => e !== "");
+        if(initialized === false){
+            list = tokens.slice(0);
+            textField.value += list.join(" ")+ "\n";
+            terminal.value = "";
+            initialized = true;
+            return
+        }
+         try {
+            functions[tokens[0]](tokens.slice(1),list,textField);
+         } catch (err){
+            if(err.message === 'functions[tokens[0]] is not a function'){
+                textField.value += 'Error: invalid command\n';
+            } else {
+                textField.value += err.message + "\n";
+            }
+         } finally {
+             terminal.value = "";
+         }
+    }
+});
