690fdb6c9ca76a4c07534f1c3808477c0917d287
bogdanovVN <hrisvn@gmail.com>
2017-07-21 15:05:17 +0300

    Conditional statement correction in reverse func for Error Handling.

--- modules/reverse.js
+++ modules/reverse.js
@@ -1,6 +1,6 @@
 define([],function (){
     return function reverse(tokens, list, textField){
-        if(tokens.length > 1 ){
+        if(tokens.length > 0 ){
             throw new Error('Error: invalid command parameters');
         }
 
